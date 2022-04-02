6d42e78a9b42a28b0472eb9718ad980a5e38a1fe
matthew-garrett <matthew.garrett252@gmail.com>
2022-04-02 12:36:10 -0400

    footer bug

--- src/components/MailChimpWrapper/MailChimpWrapper.styled.js
+++ src/components/MailChimpWrapper/MailChimpWrapper.styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const EmailWrapper = styled.div`
   flex: 1;
-  padding: 10px;
   margin: auto;
   @media screen and (max-width: 600px) {
     margin: 20px;
